27139b602292f40d23fd6c20636639022623f909
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-08-19 19:53:51 -0500

    feat: improve the JSONC example

--- examples.ts
+++ examples.ts
@@ -5,3 +5,7 @@ console.log(beno.get('example').string());
 console.log(beno.get('to').number());
 console.log(beno.get('yes').boolean());
 beno.set('xd', 'xd');
+
+const JSONC = new Beno({encoder: 'jsonc'})
+console.log(JSONC.content())
+console.log(JSONC.get('hello').string())
